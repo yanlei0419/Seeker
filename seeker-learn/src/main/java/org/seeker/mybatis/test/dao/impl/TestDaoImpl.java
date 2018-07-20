faa15b003983baf879a3bfa6f12b0cb4f34a476e
seeker <yanlei0419@163.com>
2018-07-20 10:56:41 +0800

    更新

--- seeker-learn/src/main/java/org/seeker/mybatis/test/dao/impl/TestDaoImpl.java
+++ seeker-learn/src/main/java/org/seeker/mybatis/test/dao/impl/TestDaoImpl.java
@@ -1,17 +1,17 @@
 package org.seeker.mybatis.test.dao.impl;
 
 
-import org.seeker.common.base.mybatis.db.impl.BaseDaoImpl;
-import org.seeker.mybatis.test.dao.TestDao;
-import org.seeker.mybatis.test.entity.TestEntity;
-import org.springframework.stereotype.Repository;
+//import org.seeker.common.base.mybatis.db.impl.BaseDaoImpl;
+//import org.seeker.mybatis.test.dao.TestDao;
+//import org.seeker.mybatis.test.entity.TestEntity;
+//import org.springframework.stereotype.Repository;
 
-import java.util.List;
+//import java.util.List;
 
-@Repository("testDao")public class TestDaoImpl extends BaseDaoImpl<TestEntity> implements TestDao {
-
-	public List<TestEntity> findAll() {
-		return super.getSessionTemplate().selectList(this.getStatement("listAll"));
-	}
-
-}
+//@Repository("testDao")public class TestDaoImpl extends BaseDaoImpl<TestEntity> implements TestDao {
+//
+//	public List<TestEntity> findAll() {
+//		return super.getSessionTemplate().selectList(this.getStatement("listAll"));
+//	}
+//
+//}
--- seeker-learn/src/main/java/org/seeker/mybatis/user/dao/impl/PmsUserDaoImpl.java
+++ seeker-learn/src/main/java/org/seeker/mybatis/user/dao/impl/PmsUserDaoImpl.java
@@ -1,6 +1,6 @@
 package org.seeker.mybatis.user.dao.impl;
 
-import org.seeker.common.base.mybatis.db.impl.BaseDaoImpl;
+//import org.seeker.common.base.mybatis.db.impl.BaseDaoImpl;
 import org.seeker.mybatis.user.dao.PmsUserDao;
 import org.seeker.mybatis.user.entity.PmsUser;
 import org.springframework.stereotype.Repository;
@@ -12,27 +12,27 @@ import java.util.List;
  * 
  * @描述: 用户表数据访问层接口实现类.
  */
-@Repository("pmsUserDao")
-public class PmsUserDaoImpl extends BaseDaoImpl<PmsUser> implements PmsUserDao {
-
-	/**
-	 * 根据用户登录名获取用户信息.
-	 * 
-	 * @param loginName
-	 *            .
-	 * @return user .
-	 */
-
-	public PmsUser findByUserNo(String userNo) {
-		return super.getSqlSession().selectOne(getStatement("findByUserNo"), userNo);
-	}
-
-	public List<PmsUser> findAll() {
-		return getSqlSession().selectList(getStatement("listAll"));
-	}
-	public static void main(String[] args) {
-		String sqlId="findByUserNo";
-		PmsUserDaoImpl daoImpl=new PmsUserDaoImpl();
-		daoImpl.getStatement("findByUserNo");
-	}
-}
+//@Repository("pmsUserDao")
+//public class PmsUserDaoImpl extends BaseDaoImpl<PmsUser> implements PmsUserDao {
+//
+//	/**
+//	 * 根据用户登录名获取用户信息.
+//	 *
+//	 * @param loginName
+//	 *            .
+//	 * @return user .
+//	 */
+//
+//	public PmsUser findByUserNo(String userNo) {
+//		return super.getSqlSession().selectOne(getStatement("findByUserNo"), userNo);
+//	}
+//
+//	public List<PmsUser> findAll() {
+//		return getSqlSession().selectList(getStatement("listAll"));
+//	}
+//	public static void main(String[] args) {
+//		String sqlId="findByUserNo";
+//		PmsUserDaoImpl daoImpl=new PmsUserDaoImpl();
+//		daoImpl.getStatement("findByUserNo");
+//	}
+//}
